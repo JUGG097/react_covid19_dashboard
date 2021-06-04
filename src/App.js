a0e0c20a3ad1d04fc331876367dde5916f2f0e51
unknown <adeoluwaadeboye@gmail.com>
2021-06-04 03:51:21 +0100

    :construction: Added Footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import axios from "axios";
 import loadingGif from "./img/Bean_Eater_Loading.svg";
 import BarCharts from "./components/BarCharts";
 import PieChart from "./components/PieChart";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function App() {
 	const [confirmedCases, setConfirmedCases] = useState([]);
@@ -63,6 +64,9 @@ function App() {
 			})
 			.catch((err) => {});
 	};
+
+	let d = new Date();
+	let yearDate = d.getFullYear();
 
 	useEffect(() => {
 		getCovidData();
@@ -185,6 +189,61 @@ function App() {
 									deaths={totalDeaths}
 									recovered={totalRecovered}
 								/>
+							</div>
+						</div>
+					</div>
+
+					<div
+						style={{
+							backgroundColor: "#222222",
+							borderRadius: "10px",
+							paddingTop: ".2rem",
+						}}
+					>
+						<div className="row mt-4">
+							<div className="col-sm-12 text-center">
+								<span>
+									<a href="https://github.com/JUGG097/react_covid19_dashboard">
+										<FaGithub
+											style={{
+												color: "white",
+												fontSize: "1.5rem",
+											}}
+										/>
+									</a>
+								</span>
+
+								<span className="mr-4 ml-4">
+									<a href="https://www.linkedin.com/in/adeoluwa-adeboye-051057146/">
+										<FaLinkedin
+											style={{
+												color: "#0e76a8",
+												fontSize: "1.5rem",
+											}}
+										/>
+									</a>
+								</span>
+
+								<span>
+									<a href="https://twitter.com/adeneye97">
+										<FaTwitter
+											style={{
+												color: "#1DA1F2",
+												fontSize: "1.5rem",
+											}}
+										/>
+									</a>
+								</span>
+							</div>
+						</div>
+						<div className="row mt-3">
+							<div
+								className="col-sm-12 text-center"
+								style={{
+									color: "white",
+								}}
+							>
+								<p>Copyright &copy; {yearDate}</p>
 							</div>
 						</div>
 					</div>
